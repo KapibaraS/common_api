075c9322f5013b78906d083f81fefac425a7d794
Dmytro Shevchuk <dshevc93@gmail.com>
2020-12-17 22:52:26 +0200

    Add route for editing cars

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { CarsList, NewCar } from './components/Cars';
+import { CarsList, EditCar, NewCar } from './components/Cars';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
+      <Route path="/cars/:id/edit" component={EditCar} />
       <Route path="/cars/new" component={NewCar} />
       <Route path="/" component={CarsList} />
     </Switch>
--- front/src/components/Cars/index.js
+++ front/src/components/Cars/index.js
@@ -1,2 +1,3 @@
 export { default as CarsList } from './CarsList';
 export { default as NewCar } from './NewCar';
+export { default as EditCar } from './EditCar';
